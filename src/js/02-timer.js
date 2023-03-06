734cbe4ad6605ec6f5ce9ec03c3753be30776520
DenisZaviriukha <dv.zavirukha@gmail.com>
2023-03-06 05:12:13 +0200

    2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,10 +6,23 @@ const timerInput = document.querySelector("input#datetime-picker");
 
 const startBtn = document.querySelector('button[data-start]')
 
+const timerDays = document.querySelector('span[data-days]')
+
+const timerHours = document.querySelector('span[data-hours]')
+
+const timerMinutes = document.querySelector('span[data-minutes]')
+
+const timerSeconds = document.querySelector('span[data-seconds]')
+
 startBtn.setAttribute('disabled', '')
 
 let fff = 0
-let ccc = 0
+
+let aaa = null
+ 
+let timerTimerTime = null
+
+let array = []
 
 const options = {
     enableTime: true,
@@ -22,7 +35,7 @@ const options = {
             startBtn.removeAttribute('disabled')
             }
             fff = Number(selectedDates[0]) 
-            console.log(Number(selectedDates[0]) )
+            
             // setInterval(() => { console.log(setInterval(new Date(), 1000)) }, 1000)
         }
 
@@ -38,8 +51,30 @@ const options = {
 flatpickr(timerInput, options)
 
 
+startBtn.addEventListener('click', () => {
+    startBtn.setAttribute('disabled', '')
+    setInterval(() => {
+        timerTimerTime = convertMs(fff - Number(new Date()), 1000)
+    },
+        aaa = setInterval(() => {
+            if (fff - Number(new Date()) < 0) {
+                clearInterval(timerTimerTime)
+                clearInterval(aaa)
+            }
+            else {
+                timerDays.textContent = timerTimerTime.days,
+                timerHours.textContent = timerTimerTime.hours,
+                timerMinutes.textContent = timerTimerTime.minutes,
+                timerSeconds.textContent = timerTimerTime.seconds
+            console.log((fff - Number(new Date())))    
+            }
+            
+        }, 1000))
+})
 
-startBtn.addEventListener('click', () => { setInterval(() => {console.log(convertMs())}, 1000) })
+// addLeadingZero(padStart(0))
+
+// startBtn.addEventListener('click', () => { setInterval(() => {console.log(convertMs())}, 1000) })
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -49,20 +84,22 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = pad(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = pad(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
 
-
 // console.log(Number(options.defaultDate));
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
 
 
-
+// console.log('я могу закончить?')
 
