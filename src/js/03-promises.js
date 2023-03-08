87a87386a22849ff0b09e7963d30a2aa14a3cda5
DenisZaviriukha <dv.zavirukha@gmail.com>
2023-03-08 02:22:59 +0200

    done the 3rd task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const firstDelayInput = document.querySelector('[name="delay"]');
+const stepDelayInput = document.querySelector('[name="step"]');
+const amountInput = document.querySelector('[name="amount"]');
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+
+  const delay = Number(firstDelayInput.value)
+  const step = Number(stepDelayInput.value)
+  const amount = Number(amountInput.value)
+  
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay + (i - 1) * step)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+})
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay)
+  })
 }
